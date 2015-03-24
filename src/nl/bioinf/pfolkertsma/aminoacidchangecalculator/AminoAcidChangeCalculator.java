0c77be2b46b6c07aab4cfc4a7a7ae02657e5fb99
pfolkertsma <pfolkertsma@bin231.bin>
2015-03-24 09:35:24 +0100

    hoi

--- src/nl/bioinf/pfolkertsma/aminoacidchangecalculator/AminoAcidChangeCalculator.java
+++ src/nl/bioinf/pfolkertsma/aminoacidchangecalculator/AminoAcidChangeCalculator.java
@@ -3,8 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package nl.bioinf.pfolkertsma.aminoacidchangecalculator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
@@ -16,7 +19,32 @@ public class AminoAcidChangeCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        AminoAcidChangeCalculator ac = new AminoAcidChangeCalculator();
+        ac.start();
     }
-    
+
+    public void start() {
+        csvFileReader();
+    }
+
+    public void csvFileReader() {
+        try (BufferedReader br = new BufferedReader(new FileReader("/commons/student/2014-2015/Thema11/NGnodde/CSV/testSNPdata.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                String lineElements [] = line.split(";");
+                if (lineElements[2].equals("SNV")){
+                    System.out.println(lineElements[4]);
+                    SNV snv = new SNV();
+                }
+                if (lineElements[2].equals("MNV")){
+                    System.out.println("");
+                }
+                
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
